ec166e4b66bd960cd82ee47d7acf27a44971c17c
ccunknown <pakawat_tinsirisuk@hotmail.com>
2020-09-29 15:36:59 +0700

    first commit

---
+++ src/service/scripts-service/scripts/entes-mpr45s-v2/metadata.js
@@ -0,0 +1,4 @@
+
+      const meta = {"title":"Entes MPR45S V2","description":"Add power factor to script","tags":["modbus","modbus-rtu"]};
+      module.exports = meta;
+    
--- src/service/scripts-service/scripts/entes-mpr45s/metadata.js
+++ src/service/scripts-service/scripts/entes-mpr45s/metadata.js
@@ -1,4 +1,4 @@
 
-      const meta = {"title":"Entes MPR45S","description":"test","tags":["modbus","modbus-rtu"]};
+      const meta = {"title":"Entes MPR45S","description":"Power meter script, for meter brand \"Entes\" model \"MPR45S\".","tags":["modbus","modbus-rtu"]};
       module.exports = meta;
     
