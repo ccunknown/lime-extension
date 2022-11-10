543717b73115b215f96ef4921847ee805d8244c1
ccunknown <pakawat_tinsirisuk@hotmail.com>
2022-11-10 12:06:04 +0000

    Improve timeline chart

--- static/core/js/chart-render/heat-timeline.js
+++ static/core/js/chart-render/heat-timeline.js
@@ -1,24 +1,7 @@
 const defaultOptions = {
   width: 400,
-  height: 30,
+  height: 35,
   margin: { top: 0, right: 10, bottom: 20, left: 10 },
-  key: {
-    size: `1.5rem`,
-    dy: `-0.5rem`,
-  },
-  value: {
-    size: `1.5rem`,
-    dy: `1.5rem`,
-    format: d3.format(`.2f`),
-  },
-  defaultDisplay: 0,
-  animate: {
-    pieDelay: 200,
-    pieDuration: 1500,
-    hoverEffectDuration: 100,
-    textDelay: 1500,
-    textDuration: 500,
-  },
 };
 
 /*
@@ -30,103 +13,133 @@ const defaultOptions = {
     }
   ]
   options? = {
-    defaultDisplay: `key1`,
-    unit: `%`,
-    format: `.2f`,
-    width: `150px`,
     colors: [`#FFFFFF`, `#000000`],
   }
 */
 
 function renderHeatTimeline(domId, dataArr, opt) {
   const id = domId.replace(/^#/i, ``);
+  const divId = `${id}-div-${new Date().getTime()}`;
   const options = Object.assign(defaultOptions, opt);
+  options.width = document.getElementById(`${id}`).offsetWidth;
+  let data = dataArr;
 
   console.log(`renderHeatTimeline(id: ${id})`);
   console.log(`data:`, dataArr);
   console.log(`options:`, options);
 
-  const xAccessor = (d) => d.start;
-  const yAccessor = () => 1;
-  // const yAccessor = (d) => d.end - d.start;
-
   d3.select(`#${id}`).selectAll(`*`).remove();
+  const outterDiv = d3.select(`#${id}`).append(`div`).attr(`id`, divId);
 
-  const wrapper = d3
-    .select(`#${id}`)
-    .append(`svg`)
-    .attr(`viewBox`, `0 0 ${options.width} ${options.height}`);
+  const draw = () => {
+    outterDiv.selectAll(`*`).remove();
+    const svg = outterDiv
+      .append(`svg`)
+      .attr(`viewBox`, `0 0 ${options.width} ${options.height}`);
 
-  const bounds = wrapper.append(`g`);
+    // Set scale
+    const chartStart = Math.min(...data.map((e) => e.start));
+    const chartEnd = Math.max(...data.map((e) => e.end));
+    // console.log(`chartStart:`, chartStart);
+    // console.log(`chartEnd:`, chartEnd);
+    let xScale = d3.scaleTime()
+      .domain([chartStart, chartEnd])
+      .range([options.margin.left, options.width - options.margin.right]);
+    let xScale2 = xScale.copy();
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(dataArr, () => 1))
+      .range([options.height - options.margin.bottom, 0]);
 
-  // Set scale
-  const chartStartsAt = Math.min(
-    ...dataArr.filter((e) => e.start).map((e) => e.start)
-  );
-  const chartEndsAt = Math.max(
-    ...dataArr.filter((e) => e.end).map((e) => e.end)
-  );
-  console.log(`chartStartsAt:`, chartStartsAt);
-  console.log(`chartEndsAt:`, chartEndsAt);
-  const xScale = d3.scaleTime()
-    .domain([chartStartsAt, chartEndsAt])
-    .range([options.margin.left, options.width - options.margin.right]);
-  console.log(`xScale:`, xScale);
-  const yScale = d3.scaleLinear()
-    .domain(d3.extent(dataArr, yAccessor))
-    .range([options.height - options.margin.bottom, 0]);
+    // Prepare for tooltip
+    const div = d3
+      .select("#timeline")
+      .append("div")
+      .attr(`class`, `tooltip`)
+      .style(`opacity`, 0.7)
+      .style(`visibility`, `hidden`);
 
-  // Prepare data
-  const timeBoxGenerator = d3.area()
-    // .x((d) => xScale(xAccessor(d)))
-    // .x((d) => xScale(d.start))
-    .x0((d) => xScale(d.start))
-    .x1((d) => xScale(d.end))
-    .y0(options.height - options.margin.bottom)
-    .y1((d) => yScale(yAccessor(d)))
-    .curve(d3.curveStepAfter);
+    // Time axis
+    const timeAxis = svg
+      .append(`g`)
+      .attr(
+        `transform`,
+        `translate(0, ${options.height - options.margin.bottom})`
+      )
+      .style(`font-size`, `1rem`)
+      .style(`font-weight`, `bold`)
+      .style(`color`, `#343a40`);
 
-  const div = d3.select("#timeline").append("div")
-    .attr(`class`, `tooltip`)
-    .style(`opacity`, 0.7)
-    .style(`visibility`, `hidden`);
-  const timeScale = d3.scaleTime()
-    // .domain([new Date(chartStartsAt * 1000), new Date(chartEndsAt * 1000)])
-    .domain([new Date(chartStartsAt), new Date(chartEndsAt)])
-    .range([options.margin.left, options.width - options.margin.right]);
+    // Data drawer
+    const dataAxis = svg.append(`g`);
+    const drawRect = function() {
+      dataAxis
+        .selectAll()
+        .data(data)
+        .enter()
+        .append(`rect`)
+        .attr(`x`, (d) => xScale2(d.start))
+        .attr(`y`, () => options.margin.top)
+        .attr(`width`, (d, i) => {
+          const w = xScale2(d.end) - xScale2(d.start);
+          if (Number.isNaN(w)) console.log(`NaN i[${i}]:`, d);
+          return w;
+        })
+        .attr(`height`, () => yScale(1))
+        .attr(`fill`, `#3CC692`)
+        .attr(`opacity`, 0.8);
+    };
 
-  bounds
-    .append(`g`)
-    .attr(
-      `transform`,
-      `translate(0, ${options.height - options.margin.bottom})`
-    )
-    .style(`font-size`, `0.8rem`)
-    .style(`font-weight`, `bold`)
-    .style(`color`, `#343a40`)
-    .call(d3.axisBottom(timeScale));
+    const redraw = () => {
+      timeAxis.call(d3.axisBottom(xScale2));
+      dataAxis.selectAll(`*`).remove();
+      drawRect();
+    };
 
-  // Draw data
-  // bounds
-  //   .append(`path`)
-  //   .attr(`d`, timeBoxGenerator(dataArr))
-  //   .attr(`fill`, `#3CC692`);
-  bounds
-    .selectAll(`foo`)
-    .data(dataArr)
-    .enter()
-    .append(`rect`)
-    .attr(`x`, (d) => xScale(d.start))
-    // .attr(`y`, () => options.height - options.margin.bottom)
-    .attr(`y`, () => options.margin.top)
-    .attr(`width`, (d, i) => {
-      const w = xScale(d.end) - xScale(d.start);
-      if (Number.isNaN(w)) console.log(`i[${i}]:`, d);
-      // console.log(`rect width:`, w);
-      return w;
-    })
-    .attr(`height`, () => yScale(1))
-    .attr(`fill`, `#3CC692`);
+    const zoom = d3
+      .zoom()
+      .scaleExtent([1, 10000])
+      .translateExtent([
+        [xScale.range()[0], 0],
+        [xScale.range()[1], 0],
+      ])
+      .on(`zoom`, (event) => {
+        xScale2 = event.transform.rescaleX(xScale);
+        data = dataArr
+          .map((e) => {
+            const d = JSON.parse(JSON.stringify(e));
+            if (
+              new Date(d.start).getTime() < xScale2.domain()[0].getTime() &&
+              new Date(d.end).getTime() > xScale2.domain()[0].getTime()
+            )
+              d.start = xScale2.domain()[0].getTime();
+            if (
+              new Date(d.start).getTime() < xScale2.domain()[1].getTime() &&
+              new Date(d.end).getTime() > xScale2.domain()[1].getTime()
+            )
+              d.end = xScale2.domain()[1].getTime();
+            return d;
+          })
+          .filter((e) => {
+            return (
+              new Date(e.start).getTime() >= xScale2.domain()[0].getTime() &&
+              new Date(e.end).getTime() <= xScale2.domain()[1].getTime()
+            );
+          });
+        redraw();
+      });
+    svg.call(zoom);
+    redraw();
+  };
+  draw();
+
+  const myObserver = new ResizeObserver((e) => {
+    // console.log(e);
+    console.log(e[0].contentRect.width);
+    options.width = document.getElementById(id).offsetWidth;
+    draw();
+  });
+  // myObserver.unobserve(document.getElementById(id));
+  myObserver.observe(document.getElementById(divId));
 }
 
 export default renderHeatTimeline;
