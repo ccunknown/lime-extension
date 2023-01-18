869921f12d122687bcacc60e8a0d0f6cde949fbe
ccunknown <pakawat_tinsirisuk@hotmail.com>
2023-01-18 08:22:40 +0000

    Add script: Schneider pm2200 nt standard

--- src/service/scripts-service/scripts/schneider-pm5350-nt/metadata.js
+++ src/service/scripts-service/scripts/schneider-pm5350-nt/metadata.js
@@ -1,4 +1,4 @@
 
-      const meta = {"title":"Schneider PM5350 NT Standard","description":"Script for Schneider power meter model PM5350.","tags":["modbus","modbus-rtu","modbus-tcp"]};
+      const meta = {"title":"Schneider PM5350 NT Standard","description":"Script for Schneider power meter model PM5350.","tags":["modbus","modbus-rtu","modbus-rtu-v2","modbus-tcp"]};
       module.exports = meta;
     
