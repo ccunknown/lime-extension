68ac462207db09c62ee607ad221a2c5af71fd998
ccunknown <42955685+ccunknown@users.noreply.github.com>
2025-01-06 19:13:06 +0700

    Create metadata.js

---
+++ src/service/scripts-service/scripts/demand-monitor-polling-nt/metadata.js
@@ -0,0 +1,10 @@
+{
+  "title": "Demand Monitor Polling NT Standard",
+  "description": "Script to polling data from Demand Monitor (PC Program).",
+  "tags": [
+    "modbus",
+    "modbus-tcp",
+    "modbus-rtu",
+    "modbus-rtu-v2"
+  ]
+}
